1dc885027772cbc9ba40b826e69a7b15a26f1f76
Julien Cochuyt <JulienCochuyt@users.noreply.github.com>
2018-03-27 17:07:16 +0200

    Bump version

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Web application modules support for modern or complex web sites."""),
 	# version
-	"addon_version" : "2018.03.14",
+	"addon_version" : "2018.03.27",
 	# Author(s)
 	"addon_author" : (
 		u"Frédéric Brugnot <f.brugnot@accessolutions.fr>, "
